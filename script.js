4d751e0a8f953ecad70df94cb918ae38f76788d6
Arthur <MoonKingArthur@gmail.com>
2022-07-15 22:25:29 -0400

    Change colors on button style

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function changeColor() {
-    document.body.style.background = ("#"+Math.floor(Math.random()*16777215).toString(16));
+    let color = ("#"+Math.floor(Math.random()*16777215).toString(16))
+    document.body.style.background = color;
 }
     
